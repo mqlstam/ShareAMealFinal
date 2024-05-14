7e5257e03bfe38c799ac1b68747f99458bdf9a14
mqlstam <118191161+mqlstam@users.noreply.github.com>
2024-05-14 21:49:52 +0200

    sdfdsf

--- mysql-db.js
+++ mysql-db.js
@@ -9,7 +9,7 @@ const dbConfig = {
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE || 2159021,
+  database: process.env.DB_DATABASE || '2159021',
   connectionLimit: 10,
   waitForConnections: true,
   queueLimit: 0,
